7a6e4234479677e36c3c465a29ea3acebcaef50d
SYMUJDECI <l1114016@std.yildiz.edu.tr>
2016-11-14 22:56:49 +0200

    report 2 added and some fixes in ddl and dml files

--- sg_ddl.sql
+++ sg_ddl.sql
@@ -1,15 +1,4 @@
-﻿drop table shot;
-create table shot (
-shooter_id int not null,
-shooter_ssn numeric(10) not null,
-gun_id int not null,
-field_id int not null,
-platform_num int not null,
-success_percentage int not null,
-date_time timestamp not null
-);
-
-drop table shooter;
+﻿--drop table shooter;
 create table shooter (
 id int not null,
 ssn numeric(10) not null,
@@ -19,7 +8,7 @@ communication_info varchar(100) not null,
 primary key (id, ssn)
 );
 
-drop table field;
+--drop table field;
 create table field (
 id int primary key,
 lctn varchar(100) not null,
@@ -30,21 +19,38 @@ max_range float not null,
 maintenance_info varchar(100) not null
 );
 
-drop table gun_type;
+--drop table gun_type;
 create table gun_type(
 id int primary key,
 nm varchar(20) not null,
 charge float not null,
 field_id int not null,
-ammo_precentage float not null
+ammo_percentage float not null,
+foreign key (field_id) references field
 );
 
 
-drop table gun;
+--drop table gun;
 create table gun (
 id int primary key,
 serial_number varchar(10) not null,
 nm varchar(20) not null,
 gun_type_id int not null,
-maintenance_info varchar(100) not null
+maintenance_info varchar(100) not null,
+foreign key (gun_type_id) references gun_type
+);
+
+
+--drop table shot;
+create table shot (
+shooter_id int not null,
+shooter_ssn numeric(10) not null,
+gun_id int not null,
+field_id int not null,
+platform_num int not null,
+success_percentage int not null,
+date_time timestamp not null,
+foreign key (shooter_id, shooter_ssn) references shooter,
+foreign key (gun_id) references gun,
+foreign key (field_id) references field
 );
